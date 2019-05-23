5ea9934aae8b18cc959b0baf99e4652ded67056b
ldx <kencani@qq.com>
2019-05-23 09:43:54 +0800

    更新传入的参数

--- src/main/java/org/blocktree/AddressVerify.java
+++ src/main/java/org/blocktree/AddressVerify.java
@@ -5,11 +5,11 @@ import org.blocktree.constant.Coin;
 import java.lang.reflect.Method;
 
 public class AddressVerify {
-    public static boolean Verify(String coinTag, String Params) throws Exception {
+    public static boolean Verify(String coinTag, String Address, String Params) throws Exception {
         try {
             Class<?> coinClass = Class.forName("org.blocktree.coin." + coinTag.toLowerCase() + ".Verify");
             Method method = coinClass.getMethod("Verify", String.class, String.class);
-            Object result = method.invoke(coinClass.newInstance(), coinTag, Params);
+            Object result = method.invoke(coinClass.newInstance(), Address, Params);
             boolean resultBoolean = (Boolean) result;
             return resultBoolean;
         } catch (Exception e) {
